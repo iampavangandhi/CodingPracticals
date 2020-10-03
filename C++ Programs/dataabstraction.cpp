34bf22cb33a2f3fb76114290c44975ed478bcfa7
Dhiman Seal <furyx.ds@gmail.com>
2020-10-03 15:49:53 +0530

    Typo in name fixed. Comments added in dataabstraction.cpp

---
+++ C++ Programs/dataabstraction.cpp
@@ -0,0 +1,18 @@
+#include <iostream>    
+using namespace std;    
+ class Sum {    
+	private: int x, y, z; // private variables  are providing data abstraction in this program
+	public:    
+	void add() {	// public member accessible by objects of class created
+		cout<<"Enter two numbers: ";    
+		cin>>x>>y;    
+		z= x+y;    
+		cout<<"Sum of two number is: "<<z<<endl;    
+	}    
+};
+
+int main() {    
+	Sum sm;    
+	sm.add();    
+	return 0;    
+}    
