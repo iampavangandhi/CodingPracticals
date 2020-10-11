819d46aba4c4b676619ff5b47b394bf2aa5eb48f
EJG2000 <edwinjosegeorge@gmail.com>
2020-10-11 16:45:50 +0530

    documentation added, resolved none type when last element was removed

--- Python Programs/dataStructures/BinarySearchTree.py
+++ Python Programs/dataStructures/BinarySearchTree.py
@@ -8,47 +8,55 @@ class BinaryTree:
     Class object for binary tree
     """
 
-    def __init__(self, element: int):
+    def __init__(self):
         """
-        Create a node with element, have no child nodes
+        Creates a new empty tree
 
-        >>>newNode1 = BinaryTree(4)
-        >>>newNode2 = BinaryTree(5)
+        >>>tree1 = BinaryTree()
+        >>>tree2 = BinaryTree()
         """
-        self.data = element
-        self.left = None
-        self.right = None
+        self.data = None
+        self.isEmpty = True
+        self.numberOfNodes = 0
+        self.leftTree = None
+        self.rightTree = None
 
     def add(self, element :int) -> None:
         """
         Adds element to the tree
 
-        >>>node.add(4)
-        >>>node.add(5)
+        >>>tree.add(4)
+        >>>tree.add(5)
         """
-        if element < self.data:
+        self.numberOfNodes += 1
+
+        if self.isEmpty:
+            self.data = element
+            self.isEmpty = False
+        elif element < self.data:
             # insert at left child
-            if self.left is None:
-                self.left = BinaryTree(element)
-            else:
-                self.left.add(element)
+            if self.leftTree is None:
+                self.leftTree = BinaryTree()
+            self.leftTree.add(element)
         else:
             #insert at right
-            if self.right is None:
-                self.right = BinaryTree(element)
-            else:
-                self.right.add(element)
+            if self.rightTree is None:
+                self.rightTree = BinaryTree()
+            self.rightTree.add(element)
 
     def max(self) -> int:
         """
-        Finds the maximum element from the tree
+        Finds the maximum element from the tree.
+        Raises ValueError if tree is empty
 
-        >>>element = node.max()
-        >>>print(node.max())
+        >>>element = tree.max()
+        >>>print(tree.max())
 
         """
-        if self.right is not None:
-            return self.right.max()
+        if self.isEmpty:
+            raise ValueError("Tree is empty!")
+        elif self.rightTree is not None:
+            return self.rightTree.max()
         else:
             return self.data
 
@@ -56,63 +64,91 @@ class BinaryTree:
         """
         Finds the minimum element from the tree
 
-        >>>element = node.min()
-        >>>print(node.min())
+        >>>element = tree.min()
+        >>>print(tree.min())
 
         """
-        if self.left is not None:
-            return self.left.min()
+        if self.isEmpty:
+            raise ValueError("Tree is empty!")
+        elif self.leftTree is not None:
+            return self.leftTree.min()
         else:
             return self.data
 
-    def remove(self, element :int):
+    def remove(self, element : int):
         """
-        Removes element from the tree if found
-        returns new tree
+        Removes element from the tree if found.
+        Returns tuple of updated tree and status.
 
-        >>> node1 = node1.remove(4)
-        >>> node2 = node2.remove(4)
-
+        >>> tree1,status = tree1.remove(4)
+        >>> if status:
+                print("removed")
+            else:
+                print("not removed")
         """
+        if self.isEmpty:
+            raise ValueError("Tree is empty!")
+
         if self.data == element:
             #this node has to be removed
-            if self.left is not None and self.right is not None:
-                return None
-            elif self.left is None:
-                return self.right
-            elif self.right is None:
-                return self.left
-            else:
-                rightSmallest = self.right.min()
-                self.data = rightSmallest
-                self.right = self.right.remove(rightSmallest)
-                return self
+            self.data = None
+            self.numberOfNodes -= 1
 
-        if element < self.data:
-            self.left = self.left.remove(element)
+            if self.leftTree is None and self.rightTree is None:
+                self.isEmpty = True
+                return self,True
+            elif self.leftTree is None:
+                return self.rightTree,True
+            elif self.rightTree is None:
+                return self.leftTree,True
+            else:
+                rightSmallest = self.rightTree.min()
+                self.data = rightSmallest
+                self.rightTree,status = self.rightTree.remove(rightSmallest)
+                if self.rightTree.isEmpty:
+                    self.right = None
+                return self,True
+
+        elif element < self.data and self.leftTree is not None:
+            self.leftTree,status = self.leftTree.remove(element)
+            if status:
+                self.numberOfNodes -= 1
+                if self.leftTree.isEmpty:
+                    self.leftTree = None
+            return self,status
+
+        elif self.rightTree is not None:
+            self.rightTree,status = self.rightTree.remove(element)
+            if status:
+                self.numberOfNodes -= 1
+                if self.rightTree.isEmpty:
+                    self.rightTree = None
+            return self,status
         else:
-            self.right = self.right.remove(element)
-        return self
+            return self,False
 
     def search(self, element : int) -> bool:
         """
         Returns true if element is found in tree
-        Else returns false
+        Returns false if element is not found in tree
+        Raises ValueError if tree is empty
 
-        >>>if node.search(4):
+        >>>if tree.search(4):
                print("4 found!")
-
         """
+        if self.isEmpty:
+            raise ValueError("Tree is empty!")
+
         if self.data == element:
             return True
         elif element < self.data:
-            if self.left is not None:
-                return self.left.search(element)
+            if self.leftTree is not None:
+                return self.leftTree.search(element)
             else:
                 return False
         else:
-            if self.right is not None:
-                return self.right.search(element)
+            if self.rightTree is not None:
+                return self.rightTree.search(element)
             else:
                 return False
 
@@ -120,76 +156,67 @@ class BinaryTree:
         """
         InOrder traversal : left -> current -> right
 
-        >>>node.inOrder()
+        >>>tree.inOrder()
         1 2 3
         """
-        if self.left is not None:
-            self.left.inOrder()
-        print(f"{self.data} ", end=" ")
-        if self.right is not None:
-            self.right.inOrder()
+        if self.leftTree is not None:
+            self.leftTree.inOrder()
+        if not self.isEmpty:
+            print(f"{self.data} ", end=" ")
+        if self.rightTree is not None:
+            self.rightTree.inOrder()
 
     def preOrder(self) -> None:
         """
         PreOrder traversal : current -> left -> right
 
-        >>>node.preOrder()
+        >>>tree.preOrder()
         2 1 3
         """
-        print(f"{self.data} ", end=" ")
-        if self.left is not None:
-            self.left.inOrder()
-        if self.right is not None:
-            self.right.inOrder()
+        if not self.isEmpty:
+            print(f"{self.data} ", end=" ")
+        if self.leftTree is not None:
+            self.leftTree.inOrder()
+        if self.rightTree is not None:
+            self.rightTree.inOrder()
 
     def postOrder(self) -> None:
         """
         PostOrder traversal : left -> right -> current
 
-        >>>node.postOrder()
+        >>>tree.postOrder()
         1 3 2
         """
-        if self.left is not None:
-            self.left.inOrder()
-        if self.right is not None:
-            self.right.inOrder()
-        print(f"{self.data} ", end=" ")
+        if self.leftTree is not None:
+            self.leftTree.inOrder()
+        if self.rightTree is not None:
+            self.rightTree.inOrder()
+        if not self.isEmpty:
+            print(f"{self.data} ", end=" ")
 
     def __str__(self) -> str:
         """
-        Returns the string representation of node
-        Node( data, left, right )
+        Returns the string representation of tree
+        "Binary tree of {numberOfNodes} nodes"
 
-        >>>print(node1)
-        Node(4, left , None )
-        >>>print(node2)
-        Node(2, None , right )
-        >>>print(node3)
-        Node(3, None , None )
+        >>>print(tree1)
+        Binary tree of 4 nodes
+        >>>print(tree2)
+        Binary tree of 0 nodes
         """
-        string = f"Node( {self.data}, "
-        if self.left is not None:
-            string += "left, "
-        else:
-            string += "None, "
-        if self.right is not None:
-            string += "right "
-        else:
-            string += "None "
-        string += ")"
-        return string
+        return f"Binary tree of {self.numberOfNodes} nodes"
 
 
 if __name__ == "__main__":
     """
     Sample operation on binary tree
     """
-    
-    values = [5,8,2,7,1,9,3,4,6]
 
-    BST = BinaryTree(values[0])
+    BST = BinaryTree()
     print(f"Root node details before adding elements : {BST}")
-    for element in values[1:]:
+
+    values = [5,8,2,7,1,9,3,4,6]
+    for element in values:
         BST.add(element)
     print(f"Root node details after adding elements : {BST}")
 
@@ -212,9 +239,8 @@ if __name__ == "__main__":
         print(f"Element {element} is not found")
 
     print(f"Removing {element}")
-    BST = BST.remove(element)
-
-    if BST.search(element):
-        print(f"Element {element} is found")
+    BST,status = BST.remove(element)
+    if status:
+        print(f"Element {element} is removed")
     else:
-        print(f"Element {element} is not found")
+        print(f"Element {element} is not found!")
